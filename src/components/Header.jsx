a902cbbccd51a8bc166f0a83a903e0d9f430096e
unknown <tanmayraj707@gmail.com>
2026-03-06 23:38:39 +0530

    header addition

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
     return (
         <>
             <header
-                className={`fixed top-0 shadow-2xs w-full z-50 bg-linear-to-r from-[#0f2027b7] via-[#203a43b7] to-[#2c5364b7] text-white  transition-all duration-300 
+                className={`fixed top-0 shadow-lg w-full z-50 dark:bg-[#0f172ac1] text-white  transition-all duration-300 
                     ${isScrolled ? "pt-2 pb-3" : "pt-3 pb-3.5"} px-8`}>
                 <AnimatedContent
                     distance={40}
@@ -42,7 +42,7 @@ export default function Header() {
                     delay={0}
                 >
                     <div className="hidden lg:flex justify-between items-center max-w-340 mx-auto">
-                        <div className="text-3xl font-bold text-[#bd9c2d] cursor-pointer" >Quick-Legal</div>
+                        <div className="text-3xl font-bold text-[#0195be] cursor-pointer" >Scholar-Sync</div>
 
                         <nav className="gap-8 flex">
                             {links.map((item) => (
@@ -52,10 +52,10 @@ export default function Header() {
                                     >
                                         {({ isActive }) => (
                                             <>
-                                                <span className={isActive ? "text-[#ecc137]" : "hover:text-[#caa630] transition-colors"}>
+                                                <span className={isActive ? "text-[#0195be]" : "hover:text-[#0197c1] transition-colors"}>
                                                     {item}
                                                 </span>
-                                                <span className={`absolute rounded-full -bottom-2 -left-2.5 -right-2.5 h-0.5 bg-[#bd9c2d] origin-left transition-transform duration-300 ease-out ${isActive ? "scale-x-100" : "scale-x-0 group-hover:scale-x-100"}`}></span>
+                                                <span className={`absolute rounded-full -bottom-2 -left-2.5 -right-2.5 h-0.5 bg-[#0183a6] origin-left transition-transform duration-300 ease-out ${isActive ? "scale-x-100" : "scale-x-0 group-hover:scale-x-100"}`}></span>
                                             </>
                                         )}
                                     </NavLink>
@@ -66,7 +66,7 @@ export default function Header() {
 
 
                     <div className="flex justify-between items-center lg:hidden">
-                        <div className="text-3xl font-bold text-[#bd9c2d] cursor-pointer" >Quick-Legal</div>
+                        <div className="text-3xl font-bold text-[#0195be] cursor-pointer" >Scholar-Sync</div>
                         <button
                             onClick={handlecombinedproperty}
                             className="cursor-pointer text-2xl z-50"
@@ -77,7 +77,7 @@ export default function Header() {
                     </div>
                 </AnimatedContent>
             </header>
-            <div className={`lg:hidden fixed top-14 right-0 h-screen w-64 bg-[#1d4c5c] shadow-md transition-transform duration-300 ease-in-out z-40 ${clicked ? "translate-x-0" : "translate-x-full"}`}>
+            <div className={`lg:hidden fixed top-14 right-0 h-screen w-64 bg-[#0F172A] shadow-md transition-transform duration-300 ease-in-out z-40 ${clicked ? "translate-x-0" : "translate-x-full"}`}>
                 <nav className="flex flex-col gap-7 py-20 px-8 text-xl">
                     {links.map((item) => (
                         <div key={item} className="relative group w-fit">
@@ -85,12 +85,12 @@ export default function Header() {
                                 to={item === "Home" ? "/" : `/${item.toLowerCase()}`}
                                 onClick={() => setClicked(false)}
                                 className={({ isActive }) =>
-                                    isActive ? "text-[#bd9c2d]" : "hover:text-[#caa630] transition-colors text-white"
+                                    isActive ? "text-[#0195be]" : "hover:text-[#0197c1] transition-colors text-white"
                                 }
                             >
                                 {item}
                             </NavLink>
-                            <span className="absolute rounded-full -bottom-2 -left-2.5 -right-2.5 h-0.5 bg-[#bd9c2d] origin-left scale-x-0 transition-transform duration-300 ease-out group-hover:scale-x-100"></span>
+                            <span className="absolute rounded-full -bottom-2 -left-2.5 -right-2.5 h-0.5 bg-[#0183a6] origin-left scale-x-0 transition-transform duration-300 ease-out group-hover:scale-x-100"></span>
                         </div>
                     ))}
                 </nav>
