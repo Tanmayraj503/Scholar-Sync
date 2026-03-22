961ba31fa51990c582f0b6e6b7afad6da3f0e9b2
unknown <tanmayraj707@gmail.com>
2026-03-22 23:32:31 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,9 +26,7 @@ export default function Header() {
 
     return (
         <>
-            <header
-                className={`fixed top-0 shadow-lg w-full z-50 dark:bg-[#0f172ac1] text-white  transition-all duration-300 
-                    ${isScrolled ? "pt-2 pb-3" : "pt-3 pb-3.5"} px-8`}>
+            <div className="sticky z-100 top-0">
                 <AnimatedContent
                     distance={40}
                     direction="vertical"
@@ -41,43 +39,48 @@ export default function Header() {
                     threshold={0.1}
                     delay={0}
                 >
-                    <div className="hidden lg:flex justify-between items-center max-w-340 mx-auto">
-                        <div className="text-3xl font-bold text-[#0195be] cursor-pointer" >Scholar-Sync</div>
+                    <header
+                        className={` bg-[#fbf8eca8] border-b border-[#d3d2d2] dark:border-0 shadow-md w-full dark:bg-[#0f172ac1] dark:text-white text-black  transition-all duration-300 
+                    ${isScrolled ? "pt-2 pb-3" : "pt-3 pb-3.5"} px-8`}>
 
-                        <nav className="gap-8 flex">
-                            {links.map((item) => (
-                                <div key={item} className="relative group">
-                                    <NavLink
-                                        to={item === "Home" ? "/" : `/${item.toLowerCase()}`}
-                                    >
-                                        {({ isActive }) => (
-                                            <>
-                                                <span className={isActive ? "text-[#0195be]" : "hover:text-[#0197c1] transition-colors"}>
-                                                    {item}
-                                                </span>
-                                                <span className={`absolute rounded-full -bottom-2 -left-2.5 -right-2.5 h-0.5 bg-[#0183a6] origin-left transition-transform duration-300 ease-out ${isActive ? "scale-x-100" : "scale-x-0 group-hover:scale-x-100"}`}></span>
-                                            </>
-                                        )}
-                                    </NavLink>
-                                </div>
-                            ))}
-                        </nav>
-                    </div>
+                        <div className="hidden lg:flex justify-between items-center max-w-340 mx-auto">
+                            <div className="text-3xl font-bold text-[#103998] dark:text-[#0195be] cursor-pointer" >Scholar-Sync</div>
+
+                            <nav className="gap-8 flex">
+                                {links.map((item) => (
+                                    <div key={item} className="relative group">
+                                        <NavLink
+                                            to={item === "Home" ? "/" : `/${item.toLowerCase()}`}
+                                        >
+                                            {({ isActive }) => (
+                                                <>
+                                                    <span className={isActive ? "text-[#103998] dark:text-[#0195be]" : "hover:text-[#0e3389] dark:hover:text-[#0181a4] transition-colors"}>
+                                                        {item}
+                                                    </span>
+                                                    <span className={`absolute rounded-full -bottom-2 -left-2.5 -right-2.5 h-0.5 bg-[#103998] dark:bg-[#0195be] origin-left transition-transform duration-300 ease-out ${isActive ? "scale-x-100" : "scale-x-0 group-hover:scale-x-100"}`}></span>
+                                                </>
+                                            )}
+                                        </NavLink>
+                                    </div>
+                                ))}
+                            </nav>
+                        </div>
 
 
-                    <div className="flex justify-between items-center lg:hidden">
-                        <div className="text-3xl font-bold text-[#0195be] cursor-pointer" >Scholar-Sync</div>
-                        <button
-                            onClick={handlecombinedproperty}
-                            className="cursor-pointer text-2xl z-50"
-                            aria-label="Toggle menu"
-                        >
-                            {clicked ? <FaXmark /> : <FaBars />}
-                        </button>
-                    </div>
+                        <div className="flex justify-between items-center lg:hidden">
+                            <div className="text-3xl font-bold text-[#103998] dark:text-[#0195be] cursor-pointer" >Scholar-Sync</div>
+                            <button
+                                onClick={handlecombinedproperty}
+                                className="cursor-pointer text-2xl z-50"
+                                aria-label="Toggle menu"
+                            >
+                                {clicked ? <FaXmark className="text-[#103998] dark:text-white"/> : <FaBars className="text-[#103998] dark:text-white"/>}
+                            </button>
+                        </div>
+                    </header >
                 </AnimatedContent>
-            </header>
-            <div className={`lg:hidden fixed top-14 right-0 h-screen w-64 bg-[#0F172A] shadow-md transition-transform duration-300 ease-in-out z-40 ${clicked ? "translate-x-0" : "translate-x-full"}`}>
+            </div>
+            <div className={`lg:hidden fixed ${isScrolled ? "top-14" : "top-15.5"} right-0 h-screen w-64 bg-[#ffffff] dark:bg-[#0F172A] shadow-md transition-transform duration-300 ease-in-out z-40 ${clicked ? "translate-x-0" : "translate-x-full"}`}>
                 <nav className="flex flex-col gap-7 py-20 px-8 text-xl">
                     {links.map((item) => (
                         <div key={item} className="relative group w-fit">
@@ -85,22 +88,24 @@ export default function Header() {
                                 to={item === "Home" ? "/" : `/${item.toLowerCase()}`}
                                 onClick={() => setClicked(false)}
                                 className={({ isActive }) =>
-                                    isActive ? "text-[#0195be]" : "hover:text-[#0197c1] transition-colors text-white"
+                                    isActive ? "text-[#103998] dark:text-[#0195be]" : "hover:text-[#0e3389] dark:hover:text-[#0181a4] transition-colors text-black dark:text-white"
                                 }
                             >
                                 {item}
                             </NavLink>
-                            <span className="absolute rounded-full -bottom-2 -left-2.5 -right-2.5 h-0.5 bg-[#0183a6] origin-left scale-x-0 transition-transform duration-300 ease-out group-hover:scale-x-100"></span>
+                            <span className="absolute rounded-full -bottom-2 -left-2.5 -right-2.5 h-0.5 bg-[#103998] dark:bg-[#0195be] origin-left scale-x-0 transition-transform duration-300 ease-out group-hover:scale-x-100"></span>
                         </div>
                     ))}
                 </nav>
             </div>
-            {clicked && (
-                <div
-                    className="lg:hidden fixed inset-0 z-30"
-                    onClick={() => setClicked(false)}
-                />
-            )}
+            {
+                clicked && (
+                    <div
+                        className="lg:hidden fixed inset-0 z-30"
+                        onClick={() => setClicked(false)}
+                    />
+                )
+            }
         </>
     );
 }
