ab94608b064fe64df9ab7c7a5ee897508a912c95
unknown <tanmayraj707@gmail.com>
2026-03-27 03:13:50 +0530

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,6 @@ import { AiOutlineRise } from "react-icons/ai";
 import { IoTimeOutline } from "react-icons/io5";
 import ScrollToTopButton from "./ScrollToTopButton";
 import { motion } from 'framer-motion';
-import ThemeToggle from "../Pages/darkmodetoggle";
 
 
 export default function Home() {
@@ -16,7 +15,7 @@ export default function Home() {
     const [loading, setLoading] = useState(false);
     const [analysis, setAnalysis] = useState(null);
 
-    const analyzedocument = async () => {
+    const getresources = async () => {
 
         if (!input.trim()) return;
 
@@ -40,11 +39,7 @@ export default function Home() {
             console.log("CLEAN RESPONSE:", data);
 
 
-            if (
-                typeof data.trustScore !== "number" ||
-                !Array.isArray(data.risks) ||
-                !Array.isArray(data.severity)
-            ) {
+            if (!Array.isArray(data.description)) {
                 throw new Error("Invalid response structure");
             }
 
@@ -53,9 +48,7 @@ export default function Home() {
         } catch (error) {
             console.log("Analysis error: ", error);
             setAnalysis({
-                trustScore: 0,
-                risks: ["Error analyzing document. Please try again or check the format."],
-                severity: ["high"]
+                description: ["Error analyzing document. Please try again or check the format."],
             });
         } finally {
             setLoading(false);
@@ -63,27 +56,11 @@ export default function Home() {
     };
 
 
-    const getSeveritycolor = (severity) => {
-        switch (severity) {
-            case "high": return 'text-red-600 bg-red-50 border-red-200';
-            case 'medium': return 'text-yellow-600 bg-yellow-50 border-yellow-200';
-            case 'low': return 'text-green-600 bg-green-50 border-green-200';
-            default: return 'text-gray-600 bg-gray-50 border-gray-200';
-        }
-    };
-
-    const getTrustcolor = (score) => {
-        if (score >= 80) return 'text-green-600';
-        if (score >= 50) return 'text-yellow-600';
-        return 'text-red-600';
-    };
-
     return (
         <>
             <Header />
-            <ThemeToggle />
             <div className="min-h-screen pt-10 dark:bg-[#0F172A] bg-[#fbf8ec] lg:pt-12 pb-24">
-                <div className="flex pt-14 lg:pt-16 mx-auto flex-col justify-center items-center px-7">
+                <div className="flex pt-24 lg:pt-30 mx-auto flex-col justify-center items-center px-7">
                     <motion.h1 className="text-black font-['Playfair_Display'] dark:text-[white] text-center font-bold text-[36px] sm:text-[42px] md:text-5xl mb-5"
                     initial={{opacity:0, y:20}}
                     whileInView={{opacity:1, y:0}}
@@ -139,7 +116,7 @@ export default function Home() {
                             />
                         </div>
                         <button
-                            onClick={analyzedocument}
+                            onClick={getresources}
                             disabled={loading || !input.trim()}
                             className="px-4 sm:px-16 w-full sm:w-auto bg-linear-to-r dark:bg-[#0195be] dark:hover:bg-[#0181a4] bg-[#047b33] hover:bg-[#036a2a] text-gray-300 py-4 rounded-xl font-semibold sm:text-lg  transition text-base disabled:opacity-50 disabled:cursor-not-allowed shadow-lg"
                         >
@@ -150,38 +127,24 @@ export default function Home() {
                 {analysis && (
                     <div className="px-7">
                         < div className="bg-white/5 backdrop-blur-xl border border-white/10 rounded-2xl border-opacity-20 p-6 mt-10 max-w-305 mx-auto shadow-2xl">
-                            <div className="flex items-center justify-between mb-8">
-                                <h3 className="text-2xl font-bold text-white">Analysis Results</h3>
-                                <div className="text-center">
-                                    <div className="text-sm text-purple-200 mb-1">Trust Score</div>
-                                    <div className={`text-4xl font-bold ${getTrustcolor(analysis.trustScore)}`}>
-                                        {analysis.trustScore}
-                                    </div>
-                                </div>
+                            <div className="flex mb-8">
+                                <h3 className="text-2xl font-bold text-white">Analysis Results</h3>  
                             </div>
 
                             <div className="space-y-4">
                                 <h4 className="text-xl font-semibold text-white mb-4 flex items-center gap-2">
-                                    <IoAlertCircle className="w-6 h-6 text-red-400" />
-                                    Key Risks Identified
+                                    Key desciption Identified
                                 </h4>
 
-                                {analysis.risks.map((risk, index) => (
+                                {analysis.description.map((item, index) => (
                                     <div
                                         key={index}
-                                        className={`p-4 rounded-lg border ${getSeveritycolor(analysis.severity[index])} transition-all hover:scale-102`}
-                                    >
+                                        className={`p-4 rounded-lg border transition-all hover:scale-102`}
+                                        >
                                         <div className="items-start gap-3 flex">
-                                            {analysis.severity[index] == 'high' ?
-                                                (<GoXCircleFill className="w-6 h-6 shrink-0 mt-0.5" />
-                                                ) : analysis.severity[index] == 'medium' ? (
-                                                    <IoAlertCircle className="w-6 h-6 shrink-0 mt-0.5" />
-                                                ) :
-                                                    (<FaCheckCircle className="w-6 h-6 shrink-0 mt-0.5" />)
-                                            }
+                                            
                                             <div>
-                                                <div className="font-semibold mb-1 uppercase text-xs">{analysis.severity[index]} Risk</div>
-                                                <div className="font-medium">{risk}</div>
+                                                <div className="font-medium">{item}</div>
                                             </div>
                                         </div>
                                     </div>
