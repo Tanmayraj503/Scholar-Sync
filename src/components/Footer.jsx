4b16431eef79388069b4d9f3980f5861684fb186
unknown <tanmayraj707@gmail.com>
2026-04-01 21:24:52 +0530

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,12 +7,12 @@ import { FaLinkedin } from "react-icons/fa";
 const getCurrentYear = () => new Date().getFullYear();
 export default function Footer() {
     return (
-        <section className="footer flex-col bg-[#0c1427]
- border-t border-t-gray-900 w-full flex flex-wrap justify-center  items-center">
+        <section className="footer flex-col bg-[#e8e4d6] dark:bg-[#0c1427]
+ border-t border-t-[#d3d2d2] dark:border-t-gray-900 w-full flex flex-wrap justify-center  items-center">
             <div className="flex justify-between flex-wrap md:flex-nowrap gap-5 items-center w-full pt-3 my-5 mx-auto px-7 ">
                 <div className="flex flex-col items-start justify-center max-w-137.5 md:text-start  mx-auto md:mx-0">
-                    <p className="font-bold text-3xl mb-1 text-[#0195be]">Scholar-Sync</p>
-                    <p className="text-gray-400 flex items-center flex-wrap">We read the boring stuff so you don't have to.</p>
+                    <p className="font-bold text-3xl mb-1 text-[#103998] dark:text-[#0195be]">Scholar-Sync</p>
+                    <p className="text-gray-700 dark:text-gray-400 flex items-center flex-wrap">We find the best resources for you so you dont have to keep scrolling.</p>
                 </div>
                 <div className="w-full md:w-5xl flex items-center text-center md:justify-center md:items-end flex-wrap flex-col gap-2">
                     <p className="text-gray-200 font-bold text-[19px] text-end">Let's Connect</p>
@@ -20,33 +20,33 @@ export default function Footer() {
                         <a href="https://github.com/Tanmayraj503"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="footer-icon text-gray-300 hover:text-purple-900">
+                            className="footer-icon text-gray-600 dark:text-gray-300 hover:text-purple-900">
                             <FaGithub />
 
                         </a>
                         <a href="https://www.instagram.com/tanmay.raj53"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="footer-icon text-gray-300 hover:text-pink-600">
+                            className="footer-icon text-gray-600 dark:text-gray-300 hover:text-pink-600">
                             <FaInstagram />
                         </a>
                         <a href="https://www.youtube.com/watch?v=DQS6mU8Uhn8"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="footer-icon text-gray-300 hover:text-blue-400">
+                            className="footer-icon text-gray-600 dark:text-gray-300 hover:text-blue-400">
                             <FaLinkedin />
                         </a>
                         <a href="mailto:tanmayraj707@gmail.com"
                             target="_blank"
                             rel="noopener noreferrer"
-                            className="footer-icon text-gray-300 hover:text-yellow-500">
+                            className="footer-icon text-gray-600 dark:text-gray-300 hover:text-yellow-500">
                             <FaEnvelope />
 
                         </a>
                     </div>
                 </div>
             </div>
-            <div className="text-gray-400 pb-7 text-center">© {getCurrentYear()} Tanmay Raj All rights reserved.</div>
+            <div className="text-gray-700 dark:text-gray-400 pb-7 text-center">© {getCurrentYear()} Tanmay Raj All rights reserved.</div>
         </section>
     );
 }
