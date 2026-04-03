a828f81b50f14b0237dcc1e1ff98ec10dedacfa3
unknown <tanmayraj707@gmail.com>
2026-04-03 23:28:38 +0530

    footer

---
+++ src/components/ScrollToTop.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+export default function ScrollToTop() {
+    const { pathname } = useLocation();
+
+    React.useEffect(() => {
+        scrollToTop();
+    }, [pathname]);
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            left: 0,
+        });
+    };
+
+    return null;
+};
