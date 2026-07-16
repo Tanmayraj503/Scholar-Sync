5d9c21c359231f7812e53f676e93dab8034adbbd
unknown <tanmayraj707@gmail.com>
2026-07-16 20:58:37 +0530

    lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,33 @@
-import { useState } from 'react'
+import { useState, useEffect, lazy, Suspense } from 'react'
 import Footer from './components/Footer'
 import Header from './components/Header'
-import ScrollToTop from './components/ScrollToTop'
+const ScrollToTop = lazy(() => import('./components/ScrollToTopButton'))
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './components/Home'
-import About from './Pages/About'
-import Contact from './Pages/Contact'
-
+const Home = lazy(() => import('./components/Home'))
+const About = lazy(() => import('./Pages/About'))
+const Contact = lazy(() => import('./Pages/Contact'))
+import { SearchProvider } from './components/SearchContext'
+import Loader from './components/Loader'
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
   return (
     <>
-      <BrowserRouter>
-     
-      <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="About" element={<About />} />
-          <Route path="Contact" element={<Contact />} />
-        </Routes>
-      </BrowserRouter>
+      <Suspense fallback={<Loader />}>
+        <BrowserRouter>
+          <SearchProvider>
+            <ScrollToTop />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="About" element={<About />} />
+              <Route path="Contact" element={<Contact />} />
+            </Routes>
+          </SearchProvider>
+        </BrowserRouter>
+      </Suspense>
     </>
   )
 }
