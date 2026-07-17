0db00b003c85dfdcca95f1209d7780cc1ecbd7b2
unknown <tanmayraj707@gmail.com>
2026-07-18 02:58:13 +0530

    imp main update

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './index.css'
 import App from './App.jsx'
 import Header from './components/Header.jsx'
 
+document.documentElement.classList.add("dark");
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
